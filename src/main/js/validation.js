d1d4fb21daa1431a663b7ec2a6a93547c3475c01
Ryan Chambers <ryan.b.chambers@gmail.com>
2014-02-19 07:04:55 -0500

    updated jquery, qunit, sinon

--- src/main/js/validation.js
+++ src/main/js/validation.js
@@ -18,11 +18,10 @@ var validation = function() {
 			$.ajax({
 				type : 'POST',
 				url : '/validateUserName',
-				async : false,
 				data : {
 					alias : escape(username)
-				},
-				success : function(result) {
+				}
+			}).done(function(result) {
 					var resultNode, resultMessage;
 					if (result != null) {
 						resultNode = result.getElementsByTagName('result');
@@ -37,7 +36,7 @@ var validation = function() {
 					} else {
 					}
 				}
-			});
+			);
 
 			return usernameValid;
 		},
